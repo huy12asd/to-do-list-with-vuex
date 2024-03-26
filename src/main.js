9ebe1019651a7af61244c2183704dc0279b0c04a
huy12asd <legiahuy124578@gmail.com>
2024-03-26 22:59:37 +0700

    todolist

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import Vuex from 'vuex'
+const store = new Vuex.Store()
+createApp(App).use(store).mount('#app')
